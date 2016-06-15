599f3f8f6ebd789ddfb5444e27a9c7e273d108e5
Taco Lin <taco.lin@ikala.com>
2016-06-15 10:46:50 +0800

    Define pierce core interface

--- src/straas.io/pierce/interface.go
+++ src/straas.io/pierce/interface.go
@@ -1,1 +1,27 @@
 package pierce
+
+// Pierce defines an interface Pierce core operations
+type PierceCore interface {
+	// Start starts pierce core
+	Start()
+	// Stop stops pierce core
+	Stop()
+	// Get gets data of the give room and key
+	Get(roomId, key string) (interface{}, error)
+	// Get gets all data of the give room
+	GetAll(roomId string) (map[string]interface{}, error)
+	// Set sets data of the given room and key
+	Set(roomId, key string, value interface{}) error
+	// Join adds socket connections
+	Join(SocketConnection)
+	// Leave removes socket connection
+	Leave(SocketConnection)
+}
+
+// SocketConnection defines a wrapper interface to abstract web socket
+type SocketConnection interface {
+	// RoomIds returns belonged roooms of the connection
+	RoomIds() []string
+	// Emit sends data to connection
+	Emit(data string, version uint64)
+}
